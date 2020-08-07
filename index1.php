43edc1e7fdb203e3b3865606ef7f9ad9961462c5
Ritika Shukla <rshukla.allen@gmail.com>
2020-08-07 22:40:13 +0530

    My very first commit

---
+++ index1.php
@@ -0,0 +1,7 @@
+<center>
+<h3>PHP OCR Test</h3>
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="image" />
+<input type="submit"/>
+</form>
+</center>
